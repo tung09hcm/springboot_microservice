82b6b22367eb32508dd1b11aa9e3cb5c17a54078
tung <tung.bui2004lk@hcmut.edu.vn>
2025-09-20 00:04:50 +0700

    basic api-gateway

--- api-gateway/src/main/java/com/ryo/api_gateway/route/Routes.java
+++ api-gateway/src/main/java/com/ryo/api_gateway/route/Routes.java
@@ -1,2 +1,46 @@
-package com.ryo.api_gateway.route;public class Routes {
+package com.ryo.api_gateway.route;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute(){
+        return GatewayRouterFunctions.route("productservice")
+                .route(
+                        RequestPredicates.path("/api/product"),
+                        HandlerFunctions.http(
+                                "http://localhost:8080"
+                        )
+                )
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute(){
+        return GatewayRouterFunctions.route("orderservice")
+                .route(
+                        RequestPredicates.path("/api/order"),
+                        HandlerFunctions.http(
+                                "http://localhost:8081"
+                        )
+                )
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute(){
+        return GatewayRouterFunctions.route("inventoryservice")
+                .route(
+                        RequestPredicates.path("/api/inventory"),
+                        HandlerFunctions.http(
+                                "http://localhost:8082"
+                        )
+                )
+                .build();
+    }
 }
--- orderservice/src/test/java/com/ryo/microservice/orderservice/stub/InventoryStubs.java
+++ orderservice/src/test/java/com/ryo/microservice/orderservice/stub/InventoryStubs.java
@@ -8,10 +8,18 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 public class InventoryStubs {
 
     public void stubInventoryCall(String skuCode, Integer quantity) {
-        stubFor(get(urlEqualTo("/api/inventory?skuCode=" + skuCode + "&quantity=" + quantity))
+        stubFor(
+                get(
+                        urlEqualTo(
+                                "/api/inventory?skuCode=" + skuCode
+                                        + "&quantity=" + quantity
+                        )
+                )
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
-                        .withBody("true")));
+                        .withBody("true")
+                )
+        );
     }
 }
